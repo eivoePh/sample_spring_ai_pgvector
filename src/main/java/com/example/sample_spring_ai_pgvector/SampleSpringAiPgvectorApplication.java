d8d21cf137cb0568e36ae738b6a9638026916099
hualong.jia <stane.jia@gmail.com>
2025-01-15 11:53:23 +0800

    init

---
+++ src/main/java/com/example/sample_spring_ai_pgvector/SampleSpringAiPgvectorApplication.java
@@ -0,0 +1,13 @@
+package com.example.sample_spring_ai_pgvector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringAiPgvectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringAiPgvectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sample_spring_ai_pgvector/SampleSpringAiPgvectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sample_spring_ai_pgvector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringAiPgvectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
